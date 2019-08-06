02427c3b1000c96c9c6d8292f0c9dc60c9d71255
Patrick Gartung <gartung@fnal.gov>
2019-08-06 13:17:27 -0500

    Change BUILD_REL regex

--- cms_static.py
+++ cms_static.py
@@ -1,7 +1,7 @@
 GH_CMSSW_ORGANIZATION = 'LArSoft'
 GH_CMSSW_REPO    = 'larsoft'
 GH_CMSDIST_REPO  = 'cmsdist'
-BUILD_REL        = '^[Bb]uild[ ]+(CMSSW_[^ ]+)'
+BUILD_REL        = '^[Bb]uild[ ]+LArSoft[ ]+v(_[0-9]*)*'
 NEW_ISSUE_PREFIX = 'A new Issue was created by '
 NEW_PR_PREFIX    = 'A new Pull Request was created by '
 ISSUE_SEEN_MSG   = '^A new (Pull Request|Issue) was created by '
