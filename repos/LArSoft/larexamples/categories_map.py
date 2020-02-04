95207ea20c7a2c86a4b9d9fa0ecce1a24dd25e11
Patrick Gartung <gartung@fnal.gov>
2020-02-04 11:14:27 -0600

    Add additional top level directories/files to L2

--- repos/LArSoft/larexamples/categories_map.py
+++ repos/LArSoft/larexamples/categories_map.py
@@ -1,5 +1,5 @@
 from repo_config import GH_CMSSW_REPO as gh_repo
 CMSSW_CATEGORIES = {
-  'L2': [ '%s' % gh_repo ,'CMakeLists.txt', 'test',],
+  'L2': [ '%s' % gh_repo ,'CMakeLists.txt', 'test', 'doc',],
   'L1' : [ 'ups' ]
 }
