933d786359c3f9dbaada31a9c52ca84d80c1f214
Ryunosuke O'Neil <roneil@fnal.gov>
2020-02-10 23:13:49 +0000

    some corrections

--- repos/ryuwd/Offline/comment_gh_pr.py
+++ repos/ryuwd/Offline/comment_gh_pr.py
@@ -13,7 +13,7 @@ def comment_gh_pr(repo, pr, msg):
     # The format should be:
     # line 1: SHA of the tested commit
     # line 2: the job 'context' or the string-identifier for the job
-    # line 3: job status: success/failure/error/running
+    # line 3: job status: success/failure/error/pending
     # line 4: a one-line description of the state
     # line 5: Link to detailed results/information
     # line 6+: The comment to post on the Pull Request, or, if no comment desired, add 'NOCOMMENT'
@@ -21,7 +21,7 @@ def comment_gh_pr(repo, pr, msg):
     lines = msg.split('\n')
     if len(lines) < 6: # need at least six lines.
         # Post instead the msg, and skip the git status.
-        pr.create_comment("Error: A Jenkins job did not report output in the correct format. \n\nThe output was as follows:\n%s" % msg)
+        pr.create_comment("Error: A Jenkins job did not report output in the correct format. The output was as follows:\n%s" % msg)
         return
 
     test_commit_sha = lines[0]
@@ -34,11 +34,11 @@ def comment_gh_pr(repo, pr, msg):
         state=state,
         target_url=details_link,
         description=desc,
-        context=test_suites.get_test_alias(test)
+        context=context
     )
 
 
     comment_msg = '\n'.join(lines[5:])
-
-    if not 'NOCOMMENT' in comment_msg: # some status updates by commenting might not be necessary.
+    # some status updates by commenting might not be necessary.
+    if not 'NOCOMMENT' in comment_msg:
         pr.create_comment(comment_msg)
