7c478e432184c98b0d499988d0bc3692aafb4ced
Patrick Gartung <gartung@fnal.gov>
2020-02-17 19:04:06 -0600

    Add Github object as first positional argument to comment_gh_pr

--- comment_gh_pr.py
+++ comment_gh_pr.py
@@ -1,8 +1,6 @@
 from github import Github
 
-def comment_gh_pr(repo, pr, msg):
-      from os import environ
-      gh = Github(login_or_token=environ['GITHUBTOKEN'], retry=3)
+def comment_gh_pr(gh, repo, pr, msg):
       repo = gh.get_repo(repo)
       pr   = repo.get_issue(pr)
       pr.create_comment(msg)
